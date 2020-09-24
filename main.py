e5d84b8548eedd765397fb5b3f9cc1543e328c05
Zarreen Davis <zarreen.davis@outlook.com>
2020-09-24 20:43:04 +0000

    Update code

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ number=random.randint(1,10)
 print("Well, " + myName + " I am thinking of a number between 1 and 10.")
 guess=int(input("Take a guess: "))
 if guess == number:
-  print("Good job, " +myName + "You guessed my number!") 
+  print("Good job, " +myName + "!" " You guessed my number!") 
 else:
-  print("Wrong, better luck next time!")
+  print("Wrong, better luck next time!") 
